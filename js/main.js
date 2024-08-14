8399b144939176d9bbb7eb2143ff1393d7e89cd5
Vladyslav <122152653+h4ze-07@users.noreply.github.com>
2024-08-14 17:51:30 +0300

    added gsap + catalog page (start)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// open/close menu
+
 const menuLink = document.getElementById('menu-link');
 const popupArea = document.getElementById('popup-menu');
 
@@ -9,4 +11,52 @@ popupArea.addEventListener('click', (e) => {
     if (e.target == popupArea) {
         popupArea.classList.toggle('active')
     }
+})
+
+
+//swiper
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2.5,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+
+//animations
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".title").forEach((title, index) => {
+	gsap.from(title, {
+		opacity: 0,
+		duration: 1.5,
+		x: 100,
+		scrollTrigger: {
+			trigger: title,
+			start: "top 80%",
+			end: "top 40%",
+			onUpdate: (self) => {
+				const progress = self.progress;
+				title.style.opacity = progress;
+			},
+		},
+	});
+});
+
+
+gsap.to('.big-img', {
+    width: '100%',
+    height: "1006",
+    borderRadius: 0,
+    ease: "power2.out",
+    top: 0,
+    scrollTrigger: {
+        trigger: '.big-img',
+        start: "top 70%",
+        end: "top top",
+        scrub: true,
+    },
 })
