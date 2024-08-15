543256c592dfd342bc44661eda38665cdd9f9d44
Vladyslav <122152653+h4ze-07@users.noreply.github.com>
2024-08-15 18:17:47 +0300

    added mobile menu + adaptives

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const body = document.body;
+
 // open/close menu
 
 const menuLink = document.getElementById('menu-link');
@@ -14,15 +16,37 @@ popupArea.addEventListener('click', (e) => {
 })
 
 
+//burger
+const burger = document.getElementById('burger');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    body.classList.toggle('stopScroll');
+})
+
+
 //swiper
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2.5,
-    spaceBetween: 20,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+    slidesPerView: 1.1,
+    spaceBetween: 10,
+    breakpoints: {
+        450: {
+            slidesPerView: 1.3,
+        },
+        570: {
+            slidesPerView: 1.5,
+            spaceBetween: 20,
+        },
+        640: {
+            slidesPerView: 1.8,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2.5,
+            spaceBetween: 20,
+        }
+    }
 });
 
 
@@ -33,11 +57,11 @@ gsap.utils.toArray(".title").forEach((title, index) => {
 	gsap.from(title, {
 		opacity: 0,
 		duration: 1.5,
-		x: 100,
+		x: 200,
 		scrollTrigger: {
 			trigger: title,
 			start: "top 80%",
-			end: "top 40%",
+			end: "top 50%",
 			onUpdate: (self) => {
 				const progress = self.progress;
 				title.style.opacity = progress;
@@ -47,16 +71,46 @@ gsap.utils.toArray(".title").forEach((title, index) => {
 });
 
 
-gsap.to('.big-img', {
+gsap.fromTo('.big-img', 
+    {
+        top: -50,
+        width: '231px',
+        minHeight: '120px',
+        borderRadius: '100px'
+    },
+    {
     width: '100%',
-    height: "1006",
+    height: "100%",
     borderRadius: 0,
     ease: "power2.out",
     top: 0,
     scrollTrigger: {
         trigger: '.big-img',
         start: "top 70%",
-        end: "top top",
+        end: "top 20%",
         scrub: true,
     },
 })
+ 
+
+gsap.fromTo(
+    '.big-img-mobile',
+    {
+        width: 113,
+        height: 73,
+        borderRadius: 100,
+    },
+    {
+        width: '200%',
+        height: 600,
+        left: '-50%',
+        borderRadius: 0,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: '.big-img-mobile',
+            start: "top 50%",
+            end: "top 10%",
+            scrub: true,
+        },
+    }
+);
