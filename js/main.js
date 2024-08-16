6b098557b51c9502e2f505eac91e67dd649a1d6e
Vladyslav <122152653+h4ze-07@users.noreply.github.com>
2024-08-16 13:35:58 +0300

    added single page for good

--- js/main.js
+++ js/main.js
@@ -2,55 +2,112 @@ const body = document.body;
 
 // open/close menu
 
-const menuLink = document.getElementById('menu-link');
-const popupArea = document.getElementById('popup-menu');
+const menuLink = document.getElementById("menu-link");
+const popupArea = document.getElementById("popup-menu");
 
-menuLink.addEventListener('click', () => {
-    popupArea.classList.toggle('active');
-})
+menuLink.addEventListener("click", () => {
+	popupArea.classList.toggle("active");
+});
 
-popupArea.addEventListener('click', (e) => {
-    if (e.target == popupArea) {
-        popupArea.classList.toggle('active')
-    }
-})
-
+popupArea.addEventListener("click", (e) => {
+	if (e.target == popupArea) {
+		popupArea.classList.toggle("active");
+	}
+});
 
 //burger
-const burger = document.getElementById('burger');
-const mobileMenu = document.getElementById('mobile-menu');
+const burger = document.getElementById("burger");
+const mobileMenu = document.getElementById("mobile-menu");
 
-burger.addEventListener('click', () => {
-    burger.classList.toggle('active')
-    body.classList.toggle('stopScroll');
-    mobileMenu.classList.toggle('active')
-})
-
+burger.addEventListener("click", () => {
+	burger.classList.toggle("active");
+	body.classList.toggle("stopScroll");
+	mobileMenu.classList.toggle("active");
+});
 
 //swiper
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1.1,
-    spaceBetween: 10,
+	slidesPerView: 1.1,
+	spaceBetween: 10,
+	breakpoints: {
+		450: {
+			slidesPerView: 1.3,
+		},
+		570: {
+			slidesPerView: 1.5,
+			spaceBetween: 20,
+		},
+		640: {
+			slidesPerView: 1.8,
+			spaceBetween: 20,
+		},
+		768: {
+			slidesPerView: 2.5,
+			spaceBetween: 20,
+		},
+	},
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+	slidesPerView: 1,
+	spaceBetween: 10,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
     breakpoints: {
-        450: {
-            slidesPerView: 1.3,
-        },
-        570: {
-            slidesPerView: 1.5,
+		520: {
+			slidesPerView: 1.2,
+		},
+		680: {
+			slidesPerView: 1.5,
             spaceBetween: 20,
-        },
-        640: {
-            slidesPerView: 1.8,
+		},
+		768: {
+			slidesPerView: 2,
             spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 2.5,
+		},
+		1000: {
+			slidesPerView: 3,
             spaceBetween: 20,
-        }
+		},
+		1200: {
+			slidesPerView: 4,
+            spaceBetween: 20,
+		},
     }
 });
 
+var swiper3 = new Swiper(".mySwiper3", {
+	slidesPerView: 1,
+	spaceBetween: 10,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+    breakpoints: {
+		520: {
+			slidesPerView: 1.2,
+		},
+		680: {
+			slidesPerView: 1.5,
+            spaceBetween: 20,
+		},
+		768: {
+			slidesPerView: 2,
+            spaceBetween: 20,
+		},
+		1000: {
+			slidesPerView: 3,
+            spaceBetween: 20,
+		},
+		1200: {
+			slidesPerView: 4,
+            spaceBetween: 20,
+		},
+    }
+});
 
 //animations
 gsap.registerPlugin(ScrollTrigger);
@@ -72,47 +129,47 @@ gsap.utils.toArray(".title").forEach((title, index) => {
 	});
 });
 
-
-gsap.fromTo('.big-img', 
-    {
-        top: -50,
-        width: '231px',
-        minHeight: '120px',
-        borderRadius: '100px'
-    },
-    {
-    width: '100%',
-    height: "100%",
-    borderRadius: 0,
-    ease: "power2.out",
-    top: 0,
-    scrollTrigger: {
-        trigger: '.big-img',
-        start: "top 70%",
-        end: "top 20%",
-        scrub: true,
-    },
-})
- 
+gsap.fromTo(
+	".big-img",
+	{
+		top: -50,
+		width: "231px",
+		minHeight: "120px",
+		borderRadius: "100px",
+	},
+	{
+		width: "100%",
+		height: "100%",
+		borderRadius: 0,
+		ease: "power2.out",
+		top: 0,
+		scrollTrigger: {
+			trigger: ".big-img",
+			start: "top 70%",
+			end: "top 20%",
+			scrub: true,
+		},
+	}
+);
 
 gsap.fromTo(
-    '.big-img-mobile',
-    {
-        width: 113,
-        height: 73,
-        borderRadius: 100,
-    },
-    {
-        width: '200%',
-        height: 600,
-        left: '-50%',
-        borderRadius: 0,
-        ease: "power2.out",
-        scrollTrigger: {
-            trigger: '.big-img-mobile',
-            start: "top 50%",
-            end: "top 10%",
-            scrub: true,
-        },
-    }
+	".big-img-mobile",
+	{
+		width: 220,
+		height: 73,
+		borderRadius: 100,
+	},
+	{
+		width: "200%",
+		height: 600,
+		left: "-50%",
+		borderRadius: 0,
+		ease: "power2.out",
+		scrollTrigger: {
+			trigger: ".big-img-mobile",
+			start: "top 50%",
+			end: "top 10%",
+			scrub: true,
+		},
+	}
 );
